0c39364dd2988cb8b7c43d2a669c69bc012bb7d7
aaron <calampianosamuel58@gmail.com>
2025-09-24 13:55:49 +0800

    finish
    finish

--- login.js
+++ login.js
@@ -1,6 +1,20 @@
-function  login()}
-     let email = document.getElementaryById ("email").ariaValue
+function name() {
+    
+}  login()}
+//Email: samcalampiano3@gmail.com //  Error message:Your email is invalid
+//password Samuel777 // Error message:Your email is invalid
 
-     console.log("Your Enter:"+ " + " email);
-     let password 
-    )
+     let email = document.getElementaryById ("email").ariaValue
+     let password = document.getElementaryById ("password").value
+
+     console.log("You Enter:"+ " + " email );
+     console.log("You Enter"+ " + " pass);
+
+     If (email==="")
+       console.log("TAMA ANG YUNG EMAIL")
+)else (
+     console.log("Mali ang yung email"))
+)
+
+     
+    
